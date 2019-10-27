6f27f101116e881a8f3122a6351af40f1979b53d
Michael Dreesmann <56218718+Strese64@users.noreply.github.com>
2019-10-27 10:56:01 +0100

    Update Proj_unittest.py
    Update to new Version 1.0-5 Beta

--- Proj_unittest.py
+++ Proj_unittest.py
@@ -1,7 +1,7 @@
 #   -------------------------------------------------------------------
 #   Proj    coordinate transformation class
 #           Purpose Unittest for all 4 projection functions
-#           Version 1.0-2 Beta, from 06.10.2019
+#           Version 1.0-5 Beta, from 2019-10-27
 #           Author  Dipl.-Ing. Michael Dreesmann
 #           License MIT
 #   -------------------------------------------------------------------
@@ -13,30 +13,40 @@ class ProjText(unittest.TestCase):
         # check the "LAEA"-way: 4258 -> 3035 -> 4258
         l = 12.123450
         b = 51.234560
-        t11 = Proj(von=4258, nach=3035)
-        (x1, y1) = t11.transform(l, b)
-        t12 = Proj(von=3035, nach=4258)
-        (x2, y2) = t12.transform(x1, y1)
+        t11 = Proj(fromEPSG=4258, toEPSG=3035)
+        (x1, y1, z1) = t11.transform(l, b)
+        t12 = Proj(fromEPSG=3035, toEPSG=4258)
+        (x2, y2, z2) = t12.transform(x1, y1)
+        self.assertEqual( int(abs(x2-l)*100000), 0 )
+        self.assertEqual( int(abs(y2-b)*100000), 0 )
+
+        # check the "LCC"-way: 4258 -> 3034 -> 4258
+        l = 12.123450
+        b = 51.234560
+        t11 = Proj(fromEPSG=4258, toEPSG=3034)
+        (x1, y1, z1) = t11.transform(l, b)
+        t12 = Proj(fromEPSG=3034, toEPSG=4258)
+        (x2, y2, z2) = t12.transform(x1, y1)
         self.assertEqual( int(abs(x2-l)*100000), 0 )
         self.assertEqual( int(abs(y2-b)*100000), 0 )
 
         # check the "ETRS89/32"-way: 4258 -> 3044 -> 4258
         l = 12.123450
         b = 51.234560
-        t11 = Proj(von=4258, nach=3044)
-        (x1, y1) = t11.transform(l, b)
-        t12 = Proj(von=3044, nach=4258)
-        (x2, y2) = t12.transform(x1, y1)
+        t11 = Proj(fromEPSG=4258, toEPSG=3044)
+        (x1, y1, z1) = t11.transform(l, b)
+        t12 = Proj(fromEPSG=3044, toEPSG=4258)
+        (x2, y2, z2) = t12.transform(x1, y1)
         self.assertEqual( int(abs(x2-l)*100000), 0 )
         self.assertEqual( int(abs(y2-b)*100000), 0 )
 
         # check the "ETRS89/33"-way: 4258 -> 5650 -> 4258
         l = 12.123450
         b = 51.234560
-        t11 = Proj(von=4258, nach=5650)
-        (x1, y1) = t11.transform(l, b)
-        t12 = Proj(von=5650, nach=4258)
-        (x2, y2) = t12.transform(x1, y1)
+        t11 = Proj(fromEPSG=4258, toEPSG=5650)
+        (x1, y1, z1) = t11.transform(l, b)
+        t12 = Proj(fromEPSG=5650, toEPSG=4258)
+        (x2, y2, z2) = t12.transform(x1, y1)
         self.assertEqual( int(abs(x2-l)*100000), 0 )
         self.assertEqual( int(abs(y2-b)*100000), 0 )
 				
