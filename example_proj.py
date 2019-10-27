33a59f41193fbf45e17afa862568ca1ec3771467
Michael Dreesmann <56218718+Strese64@users.noreply.github.com>
2019-10-27 09:31:37 +0100

    Update example_proj.py
    Adapt example to new version

--- example_proj.py
+++ example_proj.py
@@ -1,10 +1,16 @@
 import Proj as pj 
 
+# create a Proj-instance
 t = pj.Proj(von=4258, nach=3035)
+
+# set the longitude l and latitude b
 l = 9.54152
 b = 52.41955
-(x1,y1) = t.transform(l,b)
 
-print("Projeziere ...")
-print("EPSG:4258   l = {:.5f}  b = {:.5f}".format(l,b))
-print("EPSG:3035   x = {:.1f}  y = {:.1f}".format(x1,y1))
+# transform the coordinate to the Lambert LAEA projection
+(x1, y1, z1) = t.transform(l,b)
+
+# Output
+print("Project ...")
+print("EPSG:4258   l = {:.5f}°  b = {:.5f}°  to".format(l,b))
+print("EPSG:3035   x = {:.1f}   y = {:.1f}".format(x1,y1))
